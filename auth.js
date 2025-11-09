c4abb63a0aa869b73e9a7fbb2a3986fa115bf6c3
Joaco1244 <90734679+Joaco1244@users.noreply.github.com>
2025-11-08 18:29:34 -0800

    Fix database connection path in auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../db/connection");
+const db = require("../connection.js");
 const { issueTokenForClient } = require("../lib/auth");
 
 /**
