ddb39a329626b3ac8279774abece0b4799dcdb2b
Joaco1244 <90734679+Joaco1244@users.noreply.github.com>
2025-11-08 19:52:40 -0800

    Update authMiddleware import path in webhooks.js

--- webhooks.js
+++ webhooks.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { authMiddleware } = require("../lib/auth");
+const authMiddleware = require("./auth.js");
 const axios = require("axios");
 
 /**
