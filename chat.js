43a90fefecceab35bdc6340e9144cf124f334854
Joaco1244 <90734679+Joaco1244@users.noreply.github.com>
2025-11-08 19:36:49 -0800

    Update authMiddleware import path in chat.js

--- chat.js
+++ chat.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { authMiddleware } = require("./auth.js");
+const { authMiddleware } = require("./authMiddleware.js");
 const openai = require("./openai.js");
 const db = require("./connection.js");
 
